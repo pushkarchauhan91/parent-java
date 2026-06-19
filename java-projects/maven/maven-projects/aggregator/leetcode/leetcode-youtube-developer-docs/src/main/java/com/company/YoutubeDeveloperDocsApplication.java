abc39bedabad87e30fc68ea6fbe510a5794ca2ed
pushkarchauhan91 <pushkarchauhan91@gmail.com>
2026-06-19 10:21:01 +0530

    dp

---
+++ java-projects/maven/maven-projects/aggregator/leetcode/leetcode-youtube-developer-docs/src/main/java/com/company/YoutubeDeveloperDocsApplication.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeDeveloperDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeDeveloperDocsApplication.class, args);
+    }
+}
